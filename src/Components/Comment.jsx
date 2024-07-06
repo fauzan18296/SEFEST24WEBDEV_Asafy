f8a84f4fedfe27034ca4dea3d6a6b9cb77495647
Ahmad Fauzan <fauzan18296@gmail.com>
2024-07-06 15:26:20 +0700

    add:merubah url pada baris code axios

--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -17,7 +17,7 @@ const Comment = () => {
 
   const sendComment = (e) => {
     e.preventDefault()
-  axios.post('http://localhost:3000/comment', {
+  axios.post('https://localhost:3000/comment', {
   username: username,
   nickname: nickname,
   comment: comment
